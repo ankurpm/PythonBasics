2088aaa74a7c98aaf11d9661cb804126a629086d
ankurpm <37891760+ankurpm@users.noreply.github.com>
2025-10-01 21:51:52 -0400

    Add if-else condition exercises and demos
    Introduced exercises and Python demo scripts for basic if-else and nested if-else logic, including examples for ticket validation, voting eligibility, operator usage, and grading. These files provide foundational practice for conditional statements.

---
+++ ifelsecondition/if_else_demo.py
@@ -0,0 +1,19 @@
+'''
+if boolean_condition:
+    print("Yes, it's true")
+else:
+    print("No, it's false")
+'''
+
+isValid = False
+if isValid:
+    print("Board the train")
+else:
+    print("Invalid ticket!!")
+
+
+age = 18
+if age>=18:
+    print("You can vote")
+else:
+    print("You can't vote")
---
+++ ifelsecondition/nested_ifelse.py
@@ -0,0 +1,16 @@
+
+'''
+if marks greater than 80: Grade A
+if marks greater than 50: Grade B
+else : Fail
+'''
+
+marks = int(input("Enter your marks: "))
+
+if marks >=80:
+    print("Grade A")
+elif marks >=50:
+    print("Grade B")
+else:print("Fail")
+
+
