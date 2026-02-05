d3ad532e64239cf8810250864cb6f5ea1379eebf
ankurpm <37891760+ankurpm@users.noreply.github.com>
2026-02-04 20:27:28 -0500

    Add my_math and example Python modules
    Add new modules: modules/my_math.py with square() and cube() helper functions; modules/python_libraries.py demonstrating basic numpy usage and printing a duplicated list and an array multiplied by 2; and modules/python_modules.py demonstrating imports (math and my_math) and printing m.cube(3) and m.square(16). Includes compiled bytecode at modules/__pycache__/my_math.cpython-313.pyc. Note: my_math.py has no trailing newline.

---
+++ modules/python_libraries.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+list1 = [1,2,3,4]
+list2 = list1* 2
+arr = np.array(list1)
+
+
+result = arr * 2
+print(list2)
+print(result)
---
+++ modules/python_modules.py
@@ -0,0 +1,18 @@
+
+import math
+from math import sqrt
+from math import sqrt as s
+
+import my_math as m
+
+#print(math.pow(2,3))
+#print(sqrt(4))
+#print(s(4))
+
+print(m.cube(3))
+print(m.square(16))
+
+
+
+
+
